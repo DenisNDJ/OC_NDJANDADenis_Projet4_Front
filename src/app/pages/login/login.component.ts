0c36bd76b4cba5280720a4eaf643d16b5f212043
DenisNDJ <denis.ndjanda@groupe-esigelec.org>
2025-12-31 17:22:09 +0100

    unsubscribed loggin obs

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { SessionInformation } from 'src/app/core/models/sessionInformation.interface';
@@ -7,6 +7,8 @@ import { LoginRequest } from '../../core/models/loginRequest.interface';
 import { AuthService } from '../../core/service/auth.service';
 import {MaterialModule} from "../../shared/material.module";
 import { CommonModule } from '@angular/common';
+import { Subject } from 'rxjs/internal/Subject';
+import { takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -14,12 +16,13 @@ import { CommonModule } from '@angular/common';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, OnDestroy{
   private authService = inject(AuthService);
   private fb = inject(FormBuilder);
   private router = inject(Router);
   private sessionService = inject(SessionService);
-
+  private destroy$!: Subject<boolean>;
+  
   public hide = true;
   public onError = false;
 
@@ -40,9 +43,18 @@ export class LoginComponent {
     ]
   });
 
+  ngOnInit(): void {
+    this.destroy$ = new Subject<boolean>();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.complete();
+  }
+
   public submit(): void {
     const loginRequest = this.form.value as LoginRequest;
-    this.authService.login(loginRequest).subscribe({
+    this.authService.login(loginRequest).pipe(takeUntil(this.destroy$)).subscribe({
       next: (response: SessionInformation) => {
         this.sessionService.logIn(response);
         this.router.navigate(['/sessions']);
