ffff80de470dcbcb43d64a5f04629bdf5eed174e
DenisNDJ <denis.ndjanda@groupe-esigelec.org>
2025-12-31 17:26:43 +0100

    unsubscribe me obs

--- src/app/components/me/me.component.ts
+++ src/app/components/me/me.component.ts
@@ -6,6 +6,8 @@ import { SessionService } from '../../core/service/session.service';
 import { UserService } from '../../core/service/user.service';
 import { MaterialModule } from "../../shared/material.module";
 import { CommonModule } from "@angular/common";
+import { Subject } from 'rxjs/internal/Subject';
+import { takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-me',
@@ -18,13 +20,21 @@ export class MeComponent implements OnInit {
   private sessionService = inject(SessionService);
   private matSnackBar = inject(MatSnackBar);
   private userService = inject(UserService);
+  private destroy$!: Subject<boolean>;
   public user: User | undefined;
 
 
   ngOnInit(): void {
+    this.destroy$ = new Subject<boolean>();
     this.userService
       .getById(this.sessionService.sessionInformation!.id.toString())
+      .pipe(takeUntil(this.destroy$))
       .subscribe((user: User) => this.user = user);
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.complete();
   }
 
   public back(): void {
@@ -34,6 +44,7 @@ export class MeComponent implements OnInit {
   public delete(): void {
     this.userService
       .delete(this.sessionService.sessionInformation!.id.toString())
+      .pipe(takeUntil(this.destroy$))
       .subscribe((_) => {
         this.matSnackBar.open("Your account has been deleted !", 'Close', { duration: 3000 });
         this.sessionService.logOut();
