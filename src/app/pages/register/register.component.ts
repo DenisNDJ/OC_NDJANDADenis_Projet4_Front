5024d459ba6f5ae8dc3c943aa725e008d8e94191
DenisNDJ <denis.ndjanda@groupe-esigelec.org>
2025-12-31 17:29:23 +0100

    unsubscribe register obs

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,20 +1,24 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../core/service/auth.service';
 import { RegisterRequest } from '../../core/models/registerRequest.interface';
 import { MaterialModule } from "../../shared/material.module";
 import { CommonModule } from "@angular/common";
+import { Subject } from 'rxjs/internal/Subject';
+import { takeUntil } from 'rxjs';
+
 @Component({
   selector: 'app-register',
   imports: [CommonModule, MaterialModule],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit, OnDestroy{
   private authService = inject(AuthService);
   private fb = inject(FormBuilder);
   private router = inject(Router);
+  private destroy$!: Subject<boolean>;
   public onError = false;
 
   public form = this.fb.group({
@@ -51,10 +55,18 @@ export class RegisterComponent {
     ]
   });
 
+  ngOnInit(): void {
+    this.destroy$ = new Subject<boolean>();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.complete();
+  }
 
   public submit(): void {
     const registerRequest = this.form.value as RegisterRequest;
-    this.authService.register(registerRequest).subscribe({
+    this.authService.register(registerRequest).pipe(takeUntil(this.destroy$)).subscribe({
         next: (_: void) => this.router.navigate(['/login']),
         error: _ => this.onError = true,
       }
