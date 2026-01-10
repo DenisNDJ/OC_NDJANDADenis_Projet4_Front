a6740a35e6b330000d4d5c84bd4b5b49093320bf
DenisNDJ <denis.ndjanda@groupe-esigelec.org>
2026-01-10 21:14:10 +0100

    login remove test

--- src/app/pages/login/login.component.spec.ts
+++ src/app/pages/login/login.component.spec.ts
@@ -11,16 +11,46 @@ import { expect } from '@jest/globals';
 import { SessionService } from 'src/app/core/service/session.service';
 
 import { LoginComponent } from './login.component';
+import { AuthService } from 'src/app/core/service/auth.service';
+import { Router } from '@angular/router';
+import { SessionInformation } from 'src/app/core/models/sessionInformation.interface';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
+ const routerMock = {
+      navigate: jest.fn(),
+    };
+
+  const authServiceMock = {
+      login: jest.fn(),
+    };
+
+ const sessionServiceMock = {
+      login: jest.fn(),
+    };
+
+  const sessionInformationMock: SessionInformation = {
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9',
+    type: 'Bearer',
+    id: 1,
+    username: 'NdjandaD',
+    firstName: 'Denis',
+    lastName: 'Ndjanda',
+    admin: true,
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [LoginComponent],
-      providers: [SessionService],
+      declarations: [],
+      providers: [
+        {provide: AuthService, useValue: authServiceMock,},
+        {provide: SessionService, useValue: sessionServiceMock,},
+        {provide: Router, useValue: routerMock,},
+      ],
       imports: [
+        LoginComponent,
         RouterTestingModule,
         BrowserAnimationsModule,
         HttpClientModule,
@@ -39,4 +69,5 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
